e299ef9bba242d02c5c879f28303a5cdcb434e96
Internjunction <contactinternjunctions@gmail.com>
2024-08-04 19:01:21 +0530

    Add files via upload

---
+++ class_4.py
@@ -0,0 +1,32 @@
+"""x=10
+y=6
+#Arithmatic operator
+print("Addition=",x+y)
+print("Subtraction =",y-x)
+print("Multiplication =",x*y)
+print("Division=",x/y)
+print("Floor Division=",x//y)
+print("Remainder =",x%y)
+print("Exponential =",x**y)
+#Comparison operator
+a=98
+b=98
+print(a<b)
+print(a>b)
+print(a==b)
+print(a!=b)
+print(a>=b)
+print(a<=b)
+"""
+#Assignment operator
+x=5
+print(x)
+x+=5
+print(x)
+x-=4
+print(x)
+x*=4
+print(x)
+x//=4
+x=x//4
+print(x)
