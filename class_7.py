a9e62571e122294d404f4ef06d0ab80629473a90
Internjunction <contactinternjunctions@gmail.com>
2024-08-20 18:52:50 +0530

    Add files via upload

---
+++ class_7.py
@@ -0,0 +1,28 @@
+"""a="Debajyoti"
+print(a)
+for i in a:
+    print(i)
+#for i in a:
+    #print(a[i]) 
+
+for i in range(len(a)):
+    print(a[i])
+
+for i in range(1,10):
+    if i %5==0:
+        break
+    print(i)
+
+for i in range(0,8):
+    if i %3 ==0:
+        continue
+    print(i)    
+"""
+b= range(1,15)
+print(b)
+
+for i in b:
+    print(i)
+
+for i in range(45,40,-5):
+    print(i)
